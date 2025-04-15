41d4d1b8e89afcbdae99de631aa8bb49b0376ffe
Toni <tonigashi999@gmail.com>
2025-04-15 19:45:23 -0400

    build issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,6 @@ import localFont from "next/font/local";
 
 import Header from "@/components/ui/header";
 import Footer from "@/components/ui/footer";
-import { SessionProvider } from "next-auth/react";
-import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -57,12 +55,10 @@ export default function RootLayout({
         className={`${inter.variable} ${nacelle.variable} bg-gray-950 font-inter text-base text-gray-200 antialiased`}
       >
         <div className="flex min-h-screen flex-col overflow-hidden supports-[overflow:clip]:overflow-clip">
-          <ClerkProvider>
-            <Header />
-            {children}
+          <Header />
+          {children}
 
-            <Footer />
-          </ClerkProvider>
+          <Footer />
         </div>
       </body>
     </html>
