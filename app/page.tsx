afa260906458a4fda4d50fc5e1c243f947ab9e70
Toni Gashi <toni.gashi@lj-support.com>
2025-05-07 10:42:21 -0400

    changing title

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-export const metadata = {
-  title: "Home - Open PRO",
-  description: "Page description",
-};
-
 import PageIllustration from "@/components/page-illustration";
 import Hero from "@/components/hero-home";
 import Workflows from "@/components/workflows";
