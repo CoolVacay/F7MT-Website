4f32269bca95f72d246212e4c15a085ea8ea049c
Toni Gashi <toni.gashi@lj-support.com>
2025-05-08 11:52:11 -0400

    fixing title size

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -20,7 +20,7 @@ export default function HeroHome() {
 
             {/* Hero Heading */}
             <h1
-              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-6xl font-semibold text-transparent md:text-7xl"
+              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle font-semibold text-transparent text-4xl md:text-7xl"
               data-aos="fade-up"
             >
               Enhanced Facilities
@@ -55,14 +55,14 @@ export default function HeroHome() {
                     </span>
                   </button>
                 </div>
-                <div data-aos="fade-up" data-aos-delay={600}>
+                {/* <div data-aos="fade-up" data-aos-delay={600}>
                   <Link
                     className="btn relative w-full bg-gradient-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%] sm:ml-4 sm:w-auto"
                     href="https://app.f7taskflow.com/"
                   >
                     View Demo
                   </Link>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
